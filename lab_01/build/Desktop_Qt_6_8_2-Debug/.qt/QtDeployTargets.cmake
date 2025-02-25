52f0e209aa769dca942061df52b80866324769c4
root <root@DESKTOP-K4FCDG7>
2025-02-25 10:41:39 +0300

    deleted

--- lab_01/build/Desktop_Qt_6_8_2-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab_01_FILE /home/nuelex/Desktop/git/OOP/lab_01/build/Desktop_Qt_6_8_2-Debug/lab_01)
-set(__QT_DEPLOY_TARGET_lab_01_TYPE EXECUTABLE)
--- lab_01/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_lab_01_FILE C:/Users/Elex21/OneDrive/Desktop/Проекты/4sem/OOP/lab_01/build/Desktop_Qt_6_8_2_MinGW_64_bit-Debug/lab_01.exe)
-set(__QT_DEPLOY_TARGET_lab_01_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_lab_01_RUNTIME_DLLS C:/Qt/6.8.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.2/mingw_64/bin/Qt6Core.dll)
