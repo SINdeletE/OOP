e8b568c0c4a2eca8314189204bfca17c8021b145
nuelex <nuelex@nuelex.local_note.org>
2025-03-07 00:26:44 +0300

    almost.....

--- lab_01/mainwindow.cpp
+++ lab_01/mainwindow.cpp
@@ -42,77 +42,174 @@ void action_filename_read(action_t &action, Ui::MainWindow *ui)
     action.filename = ui->ImportFilename->text().toUtf8().constData();
 }
 
-err_t action_move_read(action_t &action, Ui::MainWindow *ui)
+err_t action_move_read_data(move_t &move, Ui::MainWindow *ui)
 {
-    move_t tmp_move;
-
     err_t res = ERR_NONE;
     bool convert_res;
 
-    tmp_move.dx = ui->MoveEditX->text().toDouble(&convert_res);
-
-    if (convert_res)
-    {
-        tmp_move.dy = ui->MoveEditY->text().toDouble(&convert_res);
-
-        if (convert_res)
-            tmp_move.dz = ui->MoveEditZ->text().toDouble(&convert_res);
-    }
+    move.dx = ui->MoveEditX->text().toDouble(&convert_res);
 
     if (! convert_res)
         res = ERR_ACTION_INVALID_ENTERED_DATA;
     else
-        action.move = tmp_move;
+    {
+        move.dy = ui->MoveEditY->text().toDouble(&convert_res);
+
+        if (! convert_res)
+            res = ERR_ACTION_INVALID_ENTERED_DATA;
+        else
+        {
+            move.dz = ui->MoveEditZ->text().toDouble(&convert_res);
+
+            if (! convert_res)
+                res = ERR_ACTION_INVALID_ENTERED_DATA;
+        }
+    }
+
+    return res;
+}
+
+err_t action_move_read(action_t &action, Ui::MainWindow *ui)
+{
+    err_t res = ERR_NONE;
+
+    res = action_move_read_data(action.move, ui);
+
+    return res;
+}
+
+err_t action_center_read_data(scale_t &scale, Ui::MainWindow *ui)
+{
+    point_t &center = scale.center;
+
+    err_t res = ERR_NONE;
+    bool convert_res;
+
+    center.x = ui->CenterEditX->text().toDouble(&convert_res);
+
+    if (! convert_res)
+        res = ERR_ACTION_INVALID_ENTERED_DATA;
+    else
+    {
+        center.y = ui->CenterEditY->text().toDouble(&convert_res);
+
+        if (! convert_res)
+            res = ERR_ACTION_INVALID_ENTERED_DATA;
+        else
+        {
+            center.z = ui->CenterEditZ->text().toDouble(&convert_res);
+
+            if (! convert_res)
+                res = ERR_ACTION_INVALID_ENTERED_DATA;
+        }
+    }
+
+    return res;
+}
+
+err_t action_scale_read_data(scale_t &scale, Ui::MainWindow *ui)
+{
+    err_t res = ERR_NONE;
+    bool convert_res;
+
+    scale.kx = ui->ScaleEditX->text().toDouble(&convert_res);
+
+    if (! convert_res)
+        res = ERR_ACTION_INVALID_ENTERED_DATA;
+    else
+    {
+        scale.ky = ui->ScaleEditY->text().toDouble(&convert_res);
+
+        if (! convert_res)
+            res = ERR_ACTION_INVALID_ENTERED_DATA;
+        else
+        {
+            scale.kz = ui->ScaleEditZ->text().toDouble(&convert_res);
+
+            if (! convert_res)
+                res = ERR_ACTION_INVALID_ENTERED_DATA;
+        }
+    }
 
     return res;
 }
 
 err_t action_scale_read(action_t &action, Ui::MainWindow *ui)
 {
-    scale_t tmp_scale;
+    err_t res = ERR_NONE;
+
+    res = action_scale_read_data(action.scale, ui);
+
+    if (res == ERR_NONE)
+        res = action_center_read_data(action.scale, ui);
+
+    return res;
+}
+
+err_t action_center_read_data(rotate_t &rotate, Ui::MainWindow *ui)
+{
+    point_t &center = rotate.center;
 
     err_t res = ERR_NONE;
     bool convert_res;
 
-    tmp_scale.kx = ui->ScaleEditX->text().toDouble(&convert_res);
-
-    if (convert_res)
-    {
-        tmp_scale.ky = ui->ScaleEditY->text().toDouble(&convert_res);
-
-        if (convert_res)
-            tmp_scale.kz = ui->ScaleEditZ->text().toDouble(&convert_res);
-    }
+    center.x = ui->CenterEditX->text().toDouble(&convert_res);
 
     if (! convert_res)
         res = ERR_ACTION_INVALID_ENTERED_DATA;
     else
-        action.scale = tmp_scale;
+    {
+        center.y = ui->CenterEditY->text().toDouble(&convert_res);
+
+        if (! convert_res)
+            res = ERR_ACTION_INVALID_ENTERED_DATA;
+        else
+        {
+            center.z = ui->CenterEditZ->text().toDouble(&convert_res);
+
+            if (! convert_res)
+                res = ERR_ACTION_INVALID_ENTERED_DATA;
+        }
+    }
+
+    return res;
+}
+
+err_t action_rotate_read_data(rotate_t &rotate, Ui::MainWindow *ui)
+{
+    err_t res = ERR_NONE;
+    bool convert_res;
+
+    rotate.ox = ui->RotateEditX->text().toDouble(&convert_res);
+
+    if (! convert_res)
+        res = ERR_ACTION_INVALID_ENTERED_DATA;
+    else
+    {
+        rotate.oy = ui->RotateEditY->text().toDouble(&convert_res);
+
+        if (! convert_res)
+            res = ERR_ACTION_INVALID_ENTERED_DATA;
+        else
+        {
+            rotate.oz = ui->RotateEditZ->text().toDouble(&convert_res);
+
+            if (! convert_res)
+                res = ERR_ACTION_INVALID_ENTERED_DATA;
+        }
+    }
 
     return res;
 }
 
 err_t action_rotate_read(action_t &action, Ui::MainWindow *ui)
 {
-    rotate_t tmp_rotate;
-
     err_t res = ERR_NONE;
-    bool convert_res;
 
-    tmp_rotate.ox = ui->RotateEditX->text().toDouble(&convert_res);
+    res = action_rotate_read_data(action.rotate, ui);
 
-    if (! convert_res)
-    {
-        tmp_rotate.oy = ui->RotateEditY->text().toDouble(&convert_res);
-
-        if (! convert_res)
-            tmp_rotate.oz = ui->RotateEditZ->text().toDouble(&convert_res);
-    }
-
-    if (! convert_res)
-        res = ERR_ACTION_INVALID_ENTERED_DATA;
-    else
-        action.rotate = tmp_rotate;
+    if (res == ERR_NONE)
+        res = action_center_read_data(action.rotate, ui);
 
     return res;
 }
--- lab_01/operation.h
+++ lab_01/operation.h
@@ -1,6 +1,7 @@
 #ifndef OPERATION_H
 #define OPERATION_H
 
+#include "point.h"
 #include "points.h"
 
 struct move_t
@@ -10,18 +11,32 @@ struct move_t
     double dz;
 };
 
-struct scale_t
+struct scale_data_t
 {
     double kx;
     double ky;
     double kz;
 };
 
-struct rotate_t
+struct scale_t
+{
+    scale_data_t data;
+
+    point_t center;
+};
+
+struct rotate_data_t
 {
     double ox;
     double oy;
     double oz;
+};
+
+struct rotate_t
+{
+    rotate_data_t data;
+
+    point_t center;
 };
 
 void operation_move(points_t &points, const move_t &move);
