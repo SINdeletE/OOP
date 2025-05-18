ee68207870274059965817f1387abf8b4bd2481b
nuelex <leha20053@gmail.com>
2025-05-18 14:51:34 +0300

    Added Camer

---
+++ lab_03/lab_03_qt/src/Action/Solutions/ActionHash.hpp
@@ -0,0 +1,21 @@
+//
+// Created by nuelex on 18/05/25.
+//
+
+#ifndef ACTIONHASH_HPP
+#define ACTIONHASH_HPP
+#include <cstddef>
+#include <memory>
+
+#include "../../Transforms/Transforms.hpp"
+
+class ActionHash
+{
+public:
+    ActionHash() = default;
+    ~ActionHash() = default;
+
+    std::size_t operator()(std::shared_ptr<BaseTransform> &transform) const { return typeid(*transform).hash_code(); }
+};
+
+#endif //ACTIONHASH_HPP
--- lab_03/lab_03_qt/src/Action/Solutions/CameraActionSolution.hpp
+++ lab_03/lab_03_qt/src/Action/Solutions/CameraActionSolution.hpp
@@ -4,12 +4,19 @@
 
 #ifndef CAMERAACTIONSOLUTION_HPP
 #define CAMERAACTIONSOLUTION_HPP
+#include <memory>
+#include <unordered_map>
 
+#include "../../Transforms/Transforms.hpp"
+#include "../CameraAction/CameraAction.hpp"
 
 
 class CameraActionSolution
 {
 
+
+private:
+    std::unordered_map<std::shared_ptr<BaseTransform>, std::shared_ptr<CameraAction>>
 };
 
 
